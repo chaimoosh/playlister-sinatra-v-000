7e0df344ce1ac1b190c8c4077f06702e41f497fa
Chaim Reisman <crreisman@gmail.com>
2018-04-17 20:35:53 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,7 +11,7 @@ class SongsController < ApplicationController
 
   post '/songs' do
     @song = Song.create(:name => params["Name"])
-    binding.pry
+    #binding.pry
     if !params["Artist Name"].empty?
       @new_artist = Artist.find_or_create_by(:name => params["Artist Name"])
       @song.artist = @new_artist
