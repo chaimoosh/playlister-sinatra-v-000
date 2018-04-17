55cf5a227b72cc2e7cc73964f05907f0fd10db97
Chaim Reisman <crreisman@gmail.com>
2018-04-17 21:59:25 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -35,7 +35,7 @@ class SongsController < ApplicationController
   end
 
   post '/songs/:slug' do
-    #binding.pry
+    binding.pry
     @song = Song.find_by_slug(params[:slug])
     if !params["Artist Name"].empty?
       Artist.update(:name => params["Artist Name"])
