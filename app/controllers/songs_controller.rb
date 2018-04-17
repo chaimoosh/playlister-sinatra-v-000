9011dc6611ba84847770ca2829f9610f8cf33968
Chaim Reisman <crreisman@gmail.com>
2018-04-17 20:26:04 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,32 @@
 class SongsController < ApplicationController
 
   get '/songs' do
+    @song_list = Song.all
     erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    #binding.pry
+    @song = Song.create(:name => params["Name"])
+    if !params["Artist Name"].empty?
+      @new_artist = Artist.find_or_create_by(:name => params["Artist Name"])
+      @song.artist = @new_artist
+    elsif !params["genres"].empty?
+      @genre = Genre.find_by_id(params["genres"])
+      @song.genre = @genre
+    end
+    @song.save
+
+    redirect "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug' do
+    @sluggish = Song.find_by_slug(params[:slug])
+  #binding.pry
+    erb :'/songs/show'
   end
 end
